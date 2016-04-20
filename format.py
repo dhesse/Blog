61198ffc2ee45391176b5d85bbb4ac7a49b05590
Dirk Hesse <dirk.hesse@intelcom.no>
2016-04-20 17:09:31 +0200

    Adding language detection.
    Adding a language tag is stll allowed and overrrides the detection.

--- format.py
+++ format.py
@@ -12,11 +12,11 @@ def extractFromIpynb(t):
                 return "".join(cell['source'])
 
 def extractFromFile(t):
-    first, last = int(t['from_line']), int(t['to_line'])
+    first, last = int(t['from-line']), int(t['to-line'])
     with open(os.path.expanduser(t['file'])) as sourceFile:
         return "".join(
             (line for i,line in enumerate(sourceFile, 1)
-             if first <= i <= last))
+             if first <= i <= last)).strip()
 
 TEMPLATE = '\n[code language="{0}"]\n{1}\n[/code]\n'
 
@@ -24,7 +24,13 @@ def extractCode(t):
     code = {'nbcell': extractFromIpynb,
             'source': extractFromFile}[t['kind']](t)
     return TEMPLATE.format(t['language'], code)
-    
+
+def detectLanguage(tag):
+    if 'language' not in tag:
+        extension = os.path.splitext(tag['file'])[1]
+        tag['language'] = {'.py': 'python',
+                           '.ipynb': 'python'}[extension]
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("markdown_file",
@@ -34,6 +40,7 @@ if __name__ == "__main__":
         ["pandoc", "-t", "html", args.markdown_file])
     html = bs4.BeautifulSoup(pandoc_output, "html.parser")
     for r in html.find_all('insert'):
+        detectLanguage(r)
         r.append(extractCode(r))
         r.unwrap()
     print html
